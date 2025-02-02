08c7c11b47790e66598dc24d82471cf61a258eb3
Samih Sghier <samihsghier2020@gmail.com>
2025-02-02 13:16:40 -0500

    s

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -33,7 +33,7 @@ export default async function HomePage() {
         <WebPageWrapper>
             <WebPageHeader
                 badge="Email Support on Autopilot"
-                title={`Email Chatbot for your business`}
+                title={`Auto Reply Email Chatbot for your business`}
                 >
                 <Balancer
                     as="p"
